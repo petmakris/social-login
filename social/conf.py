1681d113f07cb6324b9f30e8d1849135af42f950
Petros Makris <petmakris@gmail.com>
2018-11-13 16:43:03 +0200

    import rearrange

--- social/conf.py
+++ social/conf.py
@@ -4,15 +4,16 @@ import cherrypy
 import jinja2
 import json
 
-
 import json
-from pygments import highlight
-from pygments.lexers import JsonLexer
-from pygments.formatters import TerminalFormatter
 
 
 def dump(json_obj):
+    from pygments import highlight
+    from pygments.lexers import JsonLexer
+    from pygments.formatters import TerminalFormatter
+
     json_str = json.dumps(json_obj, indent=4, sort_keys=True)
+
     print(highlight(json_str, JsonLexer(), TerminalFormatter()))
 
 
